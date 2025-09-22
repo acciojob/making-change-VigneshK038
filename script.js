060863578c86cd7969d0643891d8085c0a303b45
Vignesh Kumar R <darkvignesh2002@gmail.com>
2025-09-22 12:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const makeChange = (c) => {
   // your name here
+  const quarters = Math.trunc(c / 25);
+  const dimes = Math.trunc((c - (quarters * 25)) / 10);
+  const nickles = Math.trunc((c - (quarters * 25) - (dimes * 10)) / 5);
+  const pennys = Math.trunc((c - (quarters * 25) - (dimes * 10) - (nickles * 5)) / 1);
+  return {"q": quarters, "d": dimes, "n": nickles, "p": pennys};
 };
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
